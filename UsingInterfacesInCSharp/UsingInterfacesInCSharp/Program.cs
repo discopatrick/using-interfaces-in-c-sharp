2d0cdfe164db531420ff6b2333bd97d68201aec1
discopatrick <discopatrick@gmail.com>
2018-12-03 19:00:02 +0000

    Create VS solution for Console Application

---
+++ UsingInterfacesInCSharp/UsingInterfacesInCSharp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace UsingInterfacesInCSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}

62727cb3347c098446604dbcea219d4f72fe7a87
discopatrick <discopatrick@gmail.com>
2018-12-03 19:37:16 +0000

    Add IDrawable interface defiining properties

---
+++ UsingInterfacesInCSharp/UsingInterfacesInCSharp/IDrawable.cs
@@ -0,0 +1,12 @@
+﻿using System.Drawing;
+
+namespace UsingInterfacesInCSharp
+{
+    public interface IDrawable
+    {
+        int Width { get; set; }
+        int Height { get; set; }
+        Rectangle Bounds { get; }
+        bool IsHighlighted { set; }
+    }
+}
